083aff701703b890c42534c6f6f60a94f1efb95b
Team iPredict <team.ipredictitsolutions@gmail.com>
2022-02-18 20:16:39 +0530

    [IMP] stock_barcode_customization: Call API from odoo at time of validate

--- stock_barcode_customization/models/res_config.py
+++ stock_barcode_customization/models/res_config.py
@@ -6,10 +6,10 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    url = fields.Char(related="company_id.url", readonly=False)
+    convergex_api_url = fields.Char(string="Convergex API URL", related="company_id.convergex_api_url", readonly=False)
 
 
 class Company(models.Model):
     _inherit = 'res.company'
 
-    url = fields.Char(string="URL")
+    convergex_api_url = fields.Char(string="Convergex API URL")
